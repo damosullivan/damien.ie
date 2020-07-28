781fdc15f006ee6abea53ccbc3f88a27d56afa06
Damien O'Sullivan <damosullivan@hubspot.com>
2020-07-28 12:13:49 +0100

    Migrating to Now.sh to get Server Side Rendering (SSR)

---
+++ ssr.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    template: './ssr.html',
+    maxAge: 60 * 60 * 1000,
+    config: {
+"alias": {
+          "/projects/1note-me": "https://raw.githubusercontent.com/damosullivan/1note.me/master/README.md"
+        },
+        "auto2top": true,
+        "homepage": "home.md",
+        "loadNavbar": true,
+        "loadSidebar": true,
+        "name": "damien.ie",
+        "repo": "",
+        "themeColor": "DodgerBlue",
+        "formatUpdated": "from {DD}/{MM}/{YYYY}",    }
+  }
+  
